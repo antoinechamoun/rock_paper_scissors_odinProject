c5c26c309672eab53901f9b0a8c6fb62f9916f28
Tony <tonychamoun6@gmail.com>
2022-11-25 15:19:49 +0200

    Add UI

--- javascript.js
+++ javascript.js
@@ -1,52 +1,48 @@
+let playerSelection, computerSelection;
+let counterPlayer=0;
+let counterComputer=0;
+
 function getComputerChoice(){
     let array = ['Rock', 'Paper', 'Scissors']
     let choiceNum = Math.floor(Math.random()*3)
-    return array[choiceNum]
+    computerSelection=array[choiceNum]
+    return computerSelection 
 }
 
-function checkSelection(player, computer){
-    let options = {rockHolder:"", paperHolder:"", scissorsHolder:""};
-    
-    if(player === "ROCK"){
-        options.rockHolder = "player";
-        if(computer ==="PAPER"){
-            options.paperHolder = "computer"
-        }else options.scissorsHolder = "computer"
-    }
-
-    if(player === "PAPER"){
-        options.paperHolder = "player";
-        if(computer ==="ROCK"){
-            options.rockHolder = "computer"
-        }else options.scissorsHolder = "computer"
-    }
-
-    if(player === "SCISSORS"){
-        options.scissorsHolder = "player";
-        if(computer ==="ROCK"){
-            options.rockHolder = "computer"
-        }else options.paperHolder = "computer"
-    }
-    return options
+function getPlayerSelection(e){
+        playerSelection=e.srcElement.id;
+        if(playerSelection == 'restart'){
+            location.reload()
+        }
+        return playerSelection;
 }
 
-function playRound(playerSelection, computerSelection){
-    let player = playerSelection.toUpperCase()
-    let computer = computerSelection.toUpperCase()
-    if((player === null || player==='') || (player !== "ROCK" && player!== "PAPER" && player !== "SCISSORS")){
-        console.log("You must select one choice between rock, paper or scissors");
-    }else if(player === computer){
-        console.log(`Draw! You both selected ${player}`);
+function playRound(e){
+    let player = getPlayerSelection(e).toLowerCase()
+    let computer = getComputerChoice().toLowerCase()
+    let resultPlayer = document.querySelector('.result1')
+    let resultComputer = document.querySelector('.result2')
+
+    if(!player)return
+    if(counterPlayer == 4 || counterComputer == 4){
+        window.removeEventListener('click',playRound);
+        counterPlayer==4 ? counterComputer == 4 ? alert("Same result") : alert("Congrats! You won!") : alert("Bad Luck!")
+   }else if(player === computer){
+        counterComputer++;
+        counterPlayer++;
+    }else
+    // player winning conditions
+    if(player === "rock" && computer === "scissors" ||
+       player === "paper" && computer === "rock" ||
+       player === "scissors" && computer === "paper"){
+        counterPlayer++;
     } else {
-        let options = checkSelection(player, computer)
-        if(options.rockHolder === ''){
-            console.log(options.scissorsHolder, "is the winner");
-        }
-        if(options.scissorsHolder === ''){
-            console.log(options.paperHolder, "is the winner");
-        }
-        if(options.paperHolder === ''){
-            console.log(options.rockHolder, "is the winner");
-        }
+        counterComputer++;
     }
+
+    resultPlayer.textContent = `Your result: ${counterPlayer}`;
+    resultComputer.textContent = `Computer result: ${counterComputer}`;
 }
+
+
+window.addEventListener('click', playRound)
